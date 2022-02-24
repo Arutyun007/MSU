c993fcbaccbd8610147aefff832937858e7062a4
Arutyun Martirosyan <71641567+ArutyunDev@users.noreply.github.com>
2022-02-24 17:25:12 +0300

    Create dz1.py

---
+++ Python 2022/dz1.py
@@ -0,0 +1,49 @@
+def GCD (a, b):
+    while (a != 0 and b != 0):
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    print(a + b)
+
+def eratosthenes(n):
+    my_list = []
+    for i in range(n+1):
+        my_list.append(int(1))
+    i = 2
+    while (i * i <= n):
+        if my_list[i] == 1:
+            j = i * i
+            while (j <= n):
+                my_list[j] = 0
+                j += i
+        i += 1
+    print(*[i for i in range(2,n+1) if my_list[i] == 1])
+
+def my_sqrt(n, eps):
+    x = n
+    y = (x+1.0) / 2.0
+    while y < x:
+        x = y
+        y = (x + (n / x)) / 2.0
+    #print(x)
+    t = 0
+    while (eps < 1 ):
+        eps *= 10
+        t += 1
+    #print(t)
+    print(round(x, t))
+
+n_task = int(input("Enter task number (1, 2, 3) "))
+#print(n_task)
+if n_task == 1:
+    a, b = int(input("Enter first number ")), int(input("Enter second number "))
+    GCD(a, b)
+elif n_task == 2:
+    N = int(input("Enter N "))
+    eratosthenes(N)
+elif n_task == 3:
+    N, eps = int(input("Enter N ")), float(input("Enter eps "))
+    my_sqrt(N, eps)
+else:
+    print("No such task")
